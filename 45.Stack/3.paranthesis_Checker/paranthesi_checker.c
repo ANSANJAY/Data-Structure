0c87417f86914120b8d4a17bc1e4ad2afdb19376
ANSANJAY <97059168+ANSANJAY@users.noreply.github.com>
2023-01-04 00:27:28 -0500

    removed wrong file

---
+++ 45.Stack/3.paranthesis_Checker/paranthesi_checker.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#define MAX 10
+
+
+int main()
+{
+int flag=1;	
+char exp[MAX];,
+printf("Enter the expression \n");
+fgets(exp);
+
+for(i=0 ; i<strlen(exp);i++)
+	if(exp[i]=='('||exp[i]=='{'||exp[i]=='[')
+	push(exp[i]);
+	if(exp[i]==')'||exp[i]=='}'||exp[i]==']')
+	{
+		if(top <= -1)
+	 	flag ==0;
+	else
+
+
+
+return 0;
+}
